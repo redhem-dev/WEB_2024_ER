5b1e6a3cd8d703d8dacbd0b393f08c2a4760bacc
Edhem Rogo <edhem.rogo@stu.ibu.edu.ba>
2024-06-05 07:52:51 +0200

    Done

--- WEB - PROJECT ER/js/utils/constants.js
+++ WEB - PROJECT ER/js/utils/constants.js
@@ -1,1 +1,13 @@
-var Constants = {API_BASE_URL : "http://localhost:8888/WEB/WEB - PROJECT ER/backend/"}
+var Constants = 
+{
+    // API_BASE_URL : "http://localhost:8888/WEB/WEB - PROJECT ER/backend/"
+    get_api_base_url : function(){
+        if(location.hostname === "localhost"){
+            return "http://localhost:8888/WEB/WEB - PROJECT ER/backend/";
+        }
+        else{
+            return "https://lionfish-app-acggi.ondigitalocean.app/backend/";
+        }
+    }
+
+}
